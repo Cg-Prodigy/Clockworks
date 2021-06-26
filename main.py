00dc8d6a304a2cdc65b3b7748a9a83b31462f10e
Cg-Prodigy <brightmarshall2@gmail.com>
2021-06-26 16:05:34 +0100

    Clockworks version 001

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+# Cg-Prodigy
 import pygame as py
+import math
+import time
 from pygame.locals import *
-from pygame.math import disable_swizzling
-from algo import ClockWork
-import random, math, time
-
 py.init()
 
 # global variables
@@ -14,31 +13,68 @@ CAPTION=py.display.set_caption(caption)
 FPS=py.time.Clock()
 running=True
 s_color=(36,36,36)
-#  clock variables
+#  class 
+class Clockwork:
+    def __init__(self,x,y,rad,color,screen):
+        self.x=x
+        self.y=y
+        self.x2=0
+        self.y2=0
+        self.x3=0
+        self.y3=0
+        self.x4=0
+        self.y4=0
+        self.rad=rad
+        self.color=color
+        self.screen=screen
+        self.omega=-math.pi/2
+        
+    def clock_circle(self):
+        py.draw.circle(self.screen,self.color,(self.x,self.y),self.rad,3)
+    def sec_hand(self):
+        py.draw.line(self.screen,self.color,(self.x,self.y),(self.x2,self.y2),1)
+        seconds=(time.localtime().tm_sec/(math.pi*3))
+        self.x2=self.x + math.cos(self.omega+seconds)*self.rad
+        self.y2=self.y+ math.sin(self.omega+seconds)*self.rad
+    def minute_hand(self):
+        py.draw.line(self.screen,self.color,(self.x,self.y),(self.x3,self.y3),2)
+        minutes=time.localtime().tm_min/(math.pi*3)
+        self.x3=self.x +math.cos(self.omega+minutes)*(self.rad*3/4)
+        self.y3=self.y +math.sin(self.omega+minutes)*(self.rad*3/4)
+    def hour_hand(self):
+        py.draw.line(self.screen,self.color,(self.x,self.y),(self.x4,self.y4),3)
+        hours=time.localtime().tm_hour/(math.pi*3)
+        self.x4=self.x+math.cos(self.omega+hours)*(self.rad*1/2)
+        self.y4=self.y+math.sin(self.omega+hours)*(self.rad*1/2)
 
+
+
+#  clock variables
 x= s_tuple[0]/2
 y=s_tuple[1]/2
-color1= (0,255,255)
 color2=(32,178,170)
-rad=100
+rad=150
+t_clock=Clockwork(x,y,rad,color2,SCREEN)
 
-#  cirlce
-t_clock= ClockWork(x,y,rad,SCREEN,color2)
+#  seconds variable
+color1= (0,255,255)
+s_hand=Clockwork(x,y,rad,color1,SCREEN)
 
-#  second hand
-s_hand= ClockWork(x,y,None,SCREEN,color1)
-x2= x+rad/2 +15
-y2=y+rad/2 +15
+#  minute hand variables
+color3= (255,10,168)
+m_hand=Clockwork(x,y,rad,color3,SCREEN)
+#  hour hand variables
+color4= (110, 50,230)
+h_hand=Clockwork(x,y,rad,color4,SCREEN)
+# main loop
+while running:
+    SCREEN.fill(s_color)
+    for event in py.event.get():
+        if event.type==QUIT:
+            running=False
+    t_clock.clock_circle()
+    s_hand.sec_hand()
+    m_hand.minute_hand()
+    h_hand.hour_hand()
+    py.display.update()
 
-# time
-t_seconds=time.localtime().tm_sec
-if __name__=='__main__':
-    while running:
-        FPS.tick(24)
-        SCREEN.fill(s_color)
-        for event in py.event.get():
-            if event.type==QUIT:
-                running=False
-        t_clock.theCircle()
-        s_hand.theLine(x2,y2)
-        py.display.update()
